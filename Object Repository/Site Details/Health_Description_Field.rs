a4e8eee285f4859bc7a90a008306dd5ffe7a9290
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2019-12-12 07:29:52 +0000

    Health object

---
+++ Object Repository/Site Details/Health_Description_Field.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Health_Description_Field</name>
+   <tag></tag>
+   <elementGuidId>8e140ed3-7c25-41ec-a937-573a167caafd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//textarea[@id='Site.HealthConcernsDescription']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
