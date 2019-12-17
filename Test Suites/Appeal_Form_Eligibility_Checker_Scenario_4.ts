a67b6a855269c267cad9eebb76bf47bb2e5a950b
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2019-12-17 08:42:46 +0000

    Finished planning application details (Scenario 4)

---
+++ Test Suites/Appeal_Form_Eligibility_Checker_Scenario_4.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Appeal_Form_Eligibility_Checker_Scenario_4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>37bc2775-e70f-4161-b67b-e7e3d7ca750d</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef0a21d7-bc5c-4503-bb2c-c81025c6e9a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Generic/Launch_Portal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4ce3db2-a221-4b8b-8e99-0f50e28a1130</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Eligibility Checker/Eligible_New_Appeal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>119ffaeb-b1b7-4a5d-8f4d-5508a6f26fc0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Eligibility Checker/Appeal_Now</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>92ef136e-0b56-45f4-a4ac-a98c20aa0553</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Login/Case_Officer_Appeal_Now</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e10880b-caa3-4575-92a0-71fe6616fda0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/About Planning Application/Planning application details (Scenario 4)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
