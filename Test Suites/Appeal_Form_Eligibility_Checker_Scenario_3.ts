b6374eeb79b971b307bbf901db7952e1f8cc12a4
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2019-12-17 07:34:23 +0000

    Finished Contact Details

--- Test Suites/Appeal_Form_Eligibility_Checker_Scenario_3.ts
+++ Test Suites/Appeal_Form_Eligibility_Checker_Scenario_3.ts
@@ -130,4 +130,10 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Self Service Portal/About the Appeal/Other Relevant Appeal Information (Scenario 3)</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>5e6466d2-1581-48a9-8f2e-81fb5421fda1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/About You/Contact Details</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
