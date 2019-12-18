8ba0ce1c0892cb564891860ebf2653ed2250caef
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2019-12-18 09:14:23 +0000

    Finished Environment Details (Scenario 5)

---
+++ Test Suites/Appeal_Form_Eligibility_Checker_Scenario_5.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Appeal_Form_Eligibility_Checker_Scenario_5</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c2b9cfa0-2b02-4a8b-a900-6bbb5f30802e</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef0a21d7-bc5c-4503-bb2c-c81025c6e9a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Generic/Launch_Portal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4ce3db2-a221-4b8b-8e99-0f50e28a1130</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Eligibility Checker/Eligible_New_Appeal</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>119ffaeb-b1b7-4a5d-8f4d-5508a6f26fc0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Eligibility Checker/Appeal_Now</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>92ef136e-0b56-45f4-a4ac-a98c20aa0553</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/Login/Case_Officer_Appeal_Now</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c7af94c-9c89-4aab-8689-e7810e0bb73b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/About Planning Application/Planning application details</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>60bd6dd6-aab0-4680-a384-749864fb5398</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Self Service Portal/About Planning Application/Environmental Details</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
