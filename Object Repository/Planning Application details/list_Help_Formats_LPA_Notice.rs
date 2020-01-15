64f90db30710211de048163296b07bdeb87419e3
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-01-15 18:45:07 +0000

    prod fixes

--- Object Repository/Planning Application details/list_Help_Formats_LPA_Notice.rs
+++ Object Repository/Planning Application details/list_Help_Formats_LPA_Notice.rs
@@ -6,16 +6,17 @@
    <elementGuidId>c2951c3f-2546-47c9-9de7-c6941007abda</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
+         <key>XPATH</key>
+         <value>/html[1]/body[1]/div[1]/main[1]/div[1]/div[2]/form[1]/div[1]/div[2]/fieldset[1]/div[41]/div[1]/details[1]/div[1]</value>
       </entry>
       <entry>
          <key>CSS</key>
          <value>div.govuk-width-container main.govuk-main-wrapper div.govuk-grid-row div.govuk-grid-column-full form.form-horizontal:nth-child(2) div.webform.appform.form-gopro.form-gopro-horizontal:nth-child(1) div.form-page:nth-child(3) fieldset.govuk-fieldset:nth-child(1) div.control-group.govuk-form-group:nth-child(42) div.controls details.govuk-details.govuk-hint:nth-child(5) > div.govuk-details__text</value>
       </entry>
       <entry>
-         <key>XPATH</key>
+         <key>BASIC</key>
       </entry>
    </selectorCollection>
-   <selectorMethod>CSS</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
 </WebElementEntity>
