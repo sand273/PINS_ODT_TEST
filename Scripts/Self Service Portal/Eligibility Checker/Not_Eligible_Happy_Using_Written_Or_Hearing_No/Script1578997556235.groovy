2d0b80d9ff836c0d2ad606c0744f1d014d1955f2
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-03-05 14:42:36 +0000

    Fix for ECscripts for ie

--- Scripts/Self Service Portal/Eligibility Checker/Not_Eligible_Happy_Using_Written_Or_Hearing_No/Script1578997556235.groovy
+++ Scripts/Self Service Portal/Eligibility Checker/Not_Eligible_Happy_Using_Written_Or_Hearing_No/Script1578997556235.groovy
@@ -24,6 +24,8 @@ WebUI.waitForElementClickable(findTestObject('Appeal planning decision/a_Start n
 
 WebUI.click(findTestObject('Appeal planning decision/a_Start now'))
 
+WebUI.delay(1)
+
 WebUI.waitForElementVisible(findTestObject('Appeal planning decision/end_User_Question'), 2)
 
 WebUI.verifyElementText(findTestObject('Appeal planning decision/end_User_Question'), verData.getValue(1, 1))
