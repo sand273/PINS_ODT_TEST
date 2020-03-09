6730cc35b394fda83a9d0a7fd08f967c8cf09513
sand273 <56689938+sand273@users.noreply.github.com>
2020-03-09 17:01:03 +0000

    New Test added for appeal form 2945

---
+++ Object Repository/Appeal planning decision/msessage_grant_Permission.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>msessage_grant_Permission</name>
+   <tag></tag>
+   <elementGuidId>ebfefffc-0d96-4439-ad34-6d6489ab2a41</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[contains(text(),'Upload your decision granting Permission in Princi')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Planning Application details/button_Permission_Principle.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Permission_Principle</name>
+   <tag></tag>
+   <elementGuidId>97e7e4fb-4e92-4ba0-a376-275bd82c69e1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='File.PermissionPrinciple']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
