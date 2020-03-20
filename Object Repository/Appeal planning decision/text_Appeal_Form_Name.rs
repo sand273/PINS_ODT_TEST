e8132fc05798c461a588782d48100a828f90eb56
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-03-20 13:33:05 +0000

    Adding appeal form name verification

---
+++ Object Repository/Appeal planning decision/text_Appeal_Form_Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_Appeal_Form_Name</name>
+   <tag></tag>
+   <elementGuidId>b531dde7-d505-42cd-a3d2-e4660ad8af48</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[contains(text(),'You can now submit your application on the grounds')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
