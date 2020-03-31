263058a3a48476313012e9810357f3cb9a7a11ee
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-03-31 13:40:05 +0100

    Completed Scenario 1 3567 appeal

--- Object Repository/Appeal Summary/links_Appeal_Action.rs
+++ Object Repository/Appeal Summary/links_Appeal_Action.rs
@@ -6,11 +6,11 @@
    <elementGuidId>e1ff8d5f-eb63-4aaf-acb9-96e060971cc9</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//body/div[@class='govuk-width-container']/main[@class='govuk-main-wrapper']/div[@class='govuk-grid-row']/div[@class='govuk-grid-column-one-third pins-sidebar-right']/div[3]</value>
+         <key>BASIC</key>
       </entry>
       <entry>
-         <key>BASIC</key>
+         <key>XPATH</key>
+         <value>//body/div[@class='govuk-width-container']/main[@class='govuk-main-wrapper']/div[@class='govuk-grid-row']/div[@class='govuk-grid-column-one-third pins-sidebar-right']/div[4]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
--- Scripts/Self Service Portal/Appeal Summary/Appeal Summary/Script1585238136412.groovy
+++ Scripts/Self Service Portal/Appeal Summary/Appeal Summary/Script1585238136412.groovy
@@ -14,6 +14,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
 
 def planData = TestDataFactory.findTestData('Data Files/Planning_Details')
 
