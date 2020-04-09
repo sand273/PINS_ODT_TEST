495d339be28c89411aa56a11602ee87910847793
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-04-09 12:22:34 +0100

    Completed 834

--- Scripts/Self Service Portal/About Planning Application/Planning application details (Scenario 1 - 834)/Script1581435422196.groovy
+++ Scripts/Self Service Portal/About Planning Application/Planning application details (Scenario 1 - 834)/Script1581435422196.groovy
@@ -143,8 +143,6 @@ WebUI.callTestCase(findTestCase('Self Service Portal/Generic/Upload File'), [('e
 
 WebUI.click(findTestObject('Planning Application details/button_Save_Continue'))
 
-WebUI.callTestCase(findTestCase('Self Service Portal/Generic/Application_Permission'), [('planOption') : 'Full'], FailureHandling.STOP_ON_FAILURE)
-
 WebUI.waitForElementVisible(findTestObject('Planning Application details/status_Complete_App_Details'), 10)
 
 WebUI.verifyElementText(findTestObject('Planning Application details/status_Complete_App_Details'), 'COMPLETED')
--- Scripts/Self Service Portal/About Planning Application/Planning application details (Scenario 2 - 834)/Script1581505361861.groovy
+++ Scripts/Self Service Portal/About Planning Application/Planning application details (Scenario 2 - 834)/Script1581505361861.groovy
@@ -104,12 +104,6 @@ WebUI.waitForElementVisible(findTestObject('Planning Application details/message
 WebUI.verifyElementText(findTestObject('Planning Application details/message_Upload_Application_Form'), verData.getValue(
         1, 105))
 
-WebUI.click(findTestObject('Planning Application details/link_Help_File_Format'))
-
-WebUI.waitForElementVisible(findTestObject('Planning Application details/list_Help_File_Formats'), 5)
-
-WebUI.verifyElementText(findTestObject('Planning Application details/list_Help_File_Formats'), verData.getValue(1, 32))
-
 WebUI.click(findTestObject('Planning Application details/button_Application_Form'))
 
 WebUI.callTestCase(findTestCase('Self Service Portal/Generic/Upload File'), [('exeFileName') : 'Doc_Upload.exe'], FailureHandling.STOP_ON_FAILURE)
@@ -122,13 +116,6 @@ WebUI.waitForElementVisible(findTestObject('Planning Application details/message
 
 WebUI.verifyElementText(findTestObject('Planning Application details/message_LPA_Original_Planning'), verData.getValue(1, 
         106))
-
-WebUI.click(findTestObject('Planning Application details/link_Help_Format_LPA'))
-
-WebUI.waitForElementVisible(findTestObject('Planning Application details/list_Help_Formats_LPA_Notice'), 5)
-
-WebUI.verifyElementText(findTestObject('Planning Application details/list_Help_Formats_LPA_Notice'), verData.getValue(1, 
-        32))
 
 WebUI.click(findTestObject('Planning Application details/button_LPA_Original'))
 
@@ -148,8 +135,6 @@ WebUI.click(findTestObject('Planning Application details/button_LPA_Notice'))
 WebUI.callTestCase(findTestCase('Self Service Portal/Generic/Upload File'), [('exeFileName') : 'Pdf_Upload.exe'], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.click(findTestObject('Planning Application details/button_Save_Continue'))
-
-WebUI.callTestCase(findTestCase('Self Service Portal/Generic/Application_Permission'), [('planOption') : 'Outline'], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.waitForElementVisible(findTestObject('Planning Application details/status_Complete_App_Details'), 10)
 
