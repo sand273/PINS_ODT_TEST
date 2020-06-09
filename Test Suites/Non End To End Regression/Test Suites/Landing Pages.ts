d8f7279b2e0e3126300886d4e966905ea4ef2283
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-06-09 14:42:30 +0100

    Fixed broken Regression tests

---
+++ Test Suites/Non End To End Regression/Test Suites/Landing Pages.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Landing Pages</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fcb5cb7e-8770-47a4-bc64-391789a8a9d0</testSuiteGuid>
+   <testCaseLink>
+      <guid>89591b7d-0ee8-45c6-89a8-7ba642c7e63e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GoPro UI/Regression Non End To End Tests/Landing Page (Cases)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ac195aec-6b57-429d-a484-8f92860504ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GoPro UI/Regression Non End To End Tests/Landing Page (Holding List)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
