c07c63984ab96460020298c9c11d31debae73f41
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-06-11 12:41:26 +0100

    Added Not_Eligible_Local_Authority_No_Listed_Building_Consent_8_Weeks_Less to eligibility checker

---
+++ Object Repository/Appeal planning decision/option_Listed_Building_Consent.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>option_Listed_Building_Consent</name>
+   <tag></tag>
+   <elementGuidId>58c249bb-13fc-4983-a4f6-2cffc625c965</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@value='Listed Building Consent']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
