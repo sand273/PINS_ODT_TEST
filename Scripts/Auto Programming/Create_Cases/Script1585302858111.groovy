fafbed526bcfa224f7e2e14e0fe86c8a27cbc784
Darren Stride <darren.stride@planninginspectorate.gov.uk>
2020-06-29 12:09:51 +0100

    Fixed Auto programming script

--- Scripts/Auto Programming/Create_Cases/Script1585302858111.groovy
+++ Scripts/Auto Programming/Create_Cases/Script1585302858111.groovy
@@ -482,17 +482,17 @@ for (def index : (i..rowsOnSpreadsheet)) {
 
     WebUI.verifyElementText(findTestObject('Preferred procedure/status_Complete_Preferred'), 'COMPLETED')
 
-    WebUI.waitForElementVisible(findTestObject('Technical Reports/link_Technical_Reports'), 5)
+    WebUI.waitForElementVisible(findTestObject('Application Technical Reports/link_Technical_Reports'), 5)
 
-    WebUI.click(findTestObject('Technical Reports/link_Technical_Reports'))
+    WebUI.click(findTestObject('Application Technical Reports/link_Technical_Reports'))
 
-    WebUI.waitForElementVisible(findTestObject('Technical Reports/message_Technical_Reports'), 5)
+    WebUI.waitForElementVisible(findTestObject('Application Technical Reports/message_Technical_Reports', [('index') : 1]), 5)
 
     WebUI.click(findTestObject('Planning Application details/button_Save_Continue'))
 
-    WebUI.waitForElementVisible(findTestObject('Technical Reports/status_Complete_Technical_Reports_Post_Decision'), 10)
+    WebUI.waitForElementVisible(findTestObject('Technical Reports Post Decision/status_Complete_Technical_Reports'), 10)
 
-    WebUI.verifyElementText(findTestObject('Technical Reports/status_Complete_Technical_Reports_Post_Decision'), 'COMPLETED')
+    WebUI.verifyElementText(findTestObject('Technical Reports Post Decision/status_Complete_Technical_Reports'), 'COMPLETED')
 
     WebUI.waitForElementVisible(findTestObject('Other Appeal Information/link_Other_Appeal_Information'), 5)
 
