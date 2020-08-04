d93d40cb4360d09263edebd22b8d92ca05759d4b
sand273 <56689938+sand273@users.noreply.github.com>
2020-08-04 15:03:08 +0100

    Auto programming script changes.

---
+++ Object Repository/GoPro UI/Case Summary/label_Council_Info.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_Council_Info</name>
+   <tag></tag>
+   <elementGuidId>fbcfa7d4-a7e9-4f19-a02e-9adef86b4a1f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h4[@class='ng-binding']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
