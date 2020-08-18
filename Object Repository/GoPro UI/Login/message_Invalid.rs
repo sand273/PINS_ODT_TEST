3ea3b2027e55bcfbb58af75bba9c2ff64c3a5817
sand273 <56689938+sand273@users.noreply.github.com>
2020-08-18 16:39:08 +0100

    New Test added for Picaso login checks.

---
+++ Object Repository/GoPro UI/Login/message_Invalid.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>message_Invalid</name>
+   <tag></tag>
+   <elementGuidId>551b3159-8c75-4536-92be-763ec7c4bc47</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='alert alert-warning ng-scope ng-isolate-scope']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Scripts/GoPro UI/Login/Profile Check/Script1597763331538.groovy
+++ Scripts/GoPro UI/Login/Profile Check/Script1597763331538.groovy
@@ -16,17 +16,20 @@ import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
 
 def testData = TestDataFactory.findTestData('Data Files/User_Profile')
 
+def verData = TestDataFactory.findTestData('Data Files/GoPro_UI_Verification')
+
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl(GlobalVariable.URL)
 
 WebUI.maximizeWindow()
 
+// Invalid User - Valid Password combination
 WebUI.click(findTestObject('GoPro UI/PINS GoPro/a_GoPro UI'))
 
-WebUI.waitForElementVisible(findTestObject('GoPro UI/Login/input_User name'), 5)
+WebUI.waitForElementVisible(findTestObject('GoPro UI/Login/input_User name'), 10)
 
-WebUI.setText(findTestObject('GoPro UI/Login/input_User name'), testData.getValue(16, 1))
+WebUI.setText(findTestObject('GoPro UI/Login/input_User name'), testData.getValue(17, 1))
 
 WebUI.waitForElementClickable(findTestObject('GoPro UI/Login/input_Password'), 5)
 
@@ -38,3 +41,29 @@ WebUI.setEncryptedText(findTestObject('GoPro UI/Login/input_Password'), '0cU2R0t
 
 WebUI.click(findTestObject('GoPro UI/Login/button_Login'))
 
+WebUI.waitForElementVisible(findTestObject('GoPro UI/Login/message_Invalid'), 10)
+
+WebUI.verifyElementText(findTestObject('GoPro UI/Login/message_Invalid'), verData.getValue(1, 15))
+
+WebUI.refresh()
+
+// Valid User - Invalid Password Combination
+WebUI.waitForElementVisible(findTestObject('GoPro UI/Login/input_User name'), 10)
+
+WebUI.setText(findTestObject('GoPro UI/Login/input_User name'), testData.getValue(16, 1))
+
+WebUI.waitForElementClickable(findTestObject('GoPro UI/Login/input_Password'), 5)
+
+WebUI.click(findTestObject('GoPro UI/Login/input_Password'))
+
+WebUI.delay(1)
+
+WebUI.setEncryptedText(findTestObject('GoPro UI/Login/input_Password'), '0cU2R0tWEjkRBFyzLLPPuA==')
+
+WebUI.click(findTestObject('GoPro UI/Login/button_Login'))
+
+WebUI.waitForElementVisible(findTestObject('GoPro UI/Login/message_Invalid'), 10)
+
+WebUI.verifyElementText(findTestObject('GoPro UI/Login/message_Invalid'), verData.getValue(1, 15))
+
+WebUI.closeBrowser()
