364df59d8b456dda7263ec88c4b181c302a178b7
sand273 <56689938+sand273@users.noreply.github.com>
2020-08-28 12:38:14 +0100

    Changing the verification for submit statement.

--- Scripts/Self Service Portal/LPA Statement/Submit Statement/Script1576149681464.groovy
+++ Scripts/Self Service Portal/LPA Statement/Submit Statement/Script1576149681464.groovy
@@ -78,7 +78,7 @@ WebUI.verifyElementText(findTestObject('Submit Statement/text_Appellant_Name'), 
 
 if (GlobalVariable.callTest == 'Auto-Validate')
 {
-	CustomKeywords.'custom.VerifyTable.verifyValues'('Complete Questionaire/table_Documents', expTableLPA)
+	CustomKeywords.'custom.VerifyTable.verifyTableCount'('Complete Questionaire/table_Documents', '8')
 }
 else if (GlobalVariable.callTest == 'Written')
 {
